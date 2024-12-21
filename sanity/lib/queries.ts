0929336634b4a59bd84ae967ea225bda757a4e58
Benjamin Roget <benjamin.rogetpro@gmail.com>
2024-12-21 18:02:39 +0100

    ppr startup details

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,16 +1,32 @@
 import {defineQuery} from "groq";
 
 export const STARTUPS_QUERY =
-    defineQuery('*[_type == "startup" && defined(slug.current)] | order(_createdAt desc) {\n' +
-        '    _id, \n' +
-        '    title, \n' +
-        '    slug, \n' +
-        '    _createdAt, \n' +
-        '    author -> {\n' +
-        '      _id, name, image, bio\n' +
-        '    },\n' +
-        '    views, \n' +
-        '    description, \n' +
-        '    category, \n' +
-        '    image\n' +
-        '}')
+    defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
+      _id, 
+      title, 
+      slug,
+      _createdAt,
+      author -> {
+        _id, name, image, bio
+      }, 
+      views,
+      description,
+      category,
+      image,
+    }`);
+
+export const STARTUP_BY_ID_QUERY =
+    defineQuery(`*[_type == "startup" && _id == $id][0]{
+    _id, 
+    title, 
+    slug, 
+    _createAt, 
+    author -> {
+      _id, name, image, bio
+    },
+    views, 
+    description, 
+    category, 
+    image,
+    pitch,
+}`)
